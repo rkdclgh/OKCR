e2aabeda5bc17f175c43888756e9f1b039ee351a
Chiho Kang <78959698+rkdclgh@users.noreply.github.com>
2024-11-19 09:42:08 +0900

    Update toolbox.js

--- js/toolbox.js
+++ js/toolbox.js
@@ -2,9 +2,12 @@ export function setupToolboxEvents() {
     const resetButton = document.getElementById('resetButton');
 
     resetButton.addEventListener('click', () => {
-        scale = 1;
-        translateX = 0;
-        translateY = 0;
-        updateImageTransform(document.getElementById('previewImage'));
+        const image = document.getElementById('previewImage');
+        if (image) {
+            scale = 1;
+            translateX = 0;
+            translateY = 0;
+            image.style.transform = `translate(0px, 0px) scale(1)`;
+        }
     });
 }
